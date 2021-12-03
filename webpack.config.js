d915260a830df4c3c348a82ef010803edbc6d4a1
wuchengji <17601056863@163.com>
2021-12-03 16:49:00 +0800

    基础框架搭建

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.ts',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 6688,
+    host: 'localhost',
+    open: true,
+    hot: true
+  },
   module: {
     rules: [
       {
@@ -15,8 +26,15 @@ module.exports = {
     extensions: [ '.ts', '.tsx', '.js' ]
   },
   output: {
-    filename: 'app.js',
+    filename: 'index_bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'phaser',
+      filename: 'index.html',
+      hash: true
+    })
+  ],
   mode: 'development'
 };
